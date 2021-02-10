7a13909781562ccb23b26d6a4e70057f4f45845f
Brody Bayley <brodybayley@gmail.com>
2021-02-10 02:27:09 +0000

    added isNan to check for non numbers and eslinted

--- timer1.js
+++ timer1.js
@@ -1,19 +1,15 @@
 const alarms = process.argv.slice(2);
 
-function alertTerminal(alarms) {
+const alertTerminal = alarms => {
   for (const alarm of alarms) {
-    if (alarm > 0 && alarm === alarm) {
+    const isNum = isNaN(parseInt(alarm));
+    //isNan checks to see if element is not a number
+    if (!isNum && alarm > 0) {
       setTimeout(() => {
         process.stdout.write('\x07');
-      }, alarm * 1000)
+      }, alarm * 1000);
     }
   }
-}
+};
 
 alertTerminal(alarms);
-
-
-// const alarms = [9, 7, 8, 'k']
-// const numbers = (/^[0-9]+$/);
-// const exclAlph = alarms.value.match(regex);
-// console.log(exclAlph)
